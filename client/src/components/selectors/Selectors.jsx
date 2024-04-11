748c5f98d71c366352dbf8e99b606f99674b4334
JDRengifo <josedariorf@gmail.com>
2024-04-10 23:37:48 -0300

    PI 3ra vez

---
+++ client/src/components/selectors/Selectors.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+
+function Selectors({handleOrderContinents}) {
+   
+    return(
+        <div>
+
+          <h2>Selectores</h2>
+          
+        </div>
+    )
+
+};
+export default Selectors;
