dcba37636b55eeb396539d6648f547f3e8796784
JDRengifo <josedariorf@gmail.com>
2024-04-11 19:11:10 -0300

    con PORT

--- server/index.js
+++ server/index.js
@@ -3,14 +3,11 @@ const server = require("./src/server");
 const { conn } = require('./src/db.js');
  const { getCreateBdCountry } = require("./src/controllers/getCreateBdCountry.js");
 
-const PORT = 10000;
-
+const PORT =  process.env.PORT || 3001;
   //conn.sync({ force: true}).then(() => {  //? uso Restringido
   conn.sync({ alter: true }).then(() => { 
 server.listen(PORT, () => {
-
    // getCreateBdCountry() //! FALTA ARREGLAR ESTO
-
   console.log(`Server listening on port ${PORT}`);
 })
 }).catch(error => console.error(error))
